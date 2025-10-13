303dcaa0918616ddc27bc47c154acb5c981e841f
romapark <romapark>
2025-10-13 09:47:41 +0300

    s

--- csv_to_json.py
+++ csv_to_json.py
@@ -10,8 +10,8 @@ csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vThmKqPSHyVmucq094HTn
 response = requests.get(csv_url)
 response.raise_for_status()  # varmistaa ettei virhettä tapahdu
 
-# Muutetaan vastaus tiedostomaiseksi objektiksi
-f = io.StringIO(response.text)
+# Muutetaan vastaus tiedostomaiseksi objektiksi UTF-8:lla
+f = io.StringIO(response.content.decode('utf-8'))
 reader = csv.DictReader(f)
 
 data = []
@@ -28,8 +28,8 @@ for row in reader:
 
     data.append(clean_row)
 
-# Tallennetaan JSON
-json_file = "testi_resorts.json"
+# Tallennetaan JSON UTF-8:lla ja ilman Unicode-escapeja
+json_file = "ski_resorts.json"
 with open(json_file, "w", encoding="utf-8") as f_out:
     json.dump(data, f_out, ensure_ascii=False, indent=2)
 
